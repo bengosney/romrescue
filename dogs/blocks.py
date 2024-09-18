12dbec2dca545baaa6e3dd925d0d30d53c0a3c34
Ben Gosney <bengosney@googlemail.com>
2024-09-18 21:38:52 +0100

    feat(dog): dog styleing

--- dogs/blocks.py
+++ dogs/blocks.py
@@ -7,8 +7,6 @@ class DogListBlock(blocks.StaticBlock):
     class Meta:
         template = "dogs/blocks/dog_list_block.html"
 
-    title = blocks.CharBlock(required=True)
-
     def get_context(self, value, parent_context=None):
         context = super().get_context(value, parent_context)
         page = context.get("page", None)
--- dogs/models.py
+++ dogs/models.py
@@ -1,26 +1,23 @@
 from django.db import models
 from modelcluster.fields import ParentalKey
 from wagtail.admin.panels import FieldPanel, InlinePanel
+from wagtail.fields import RichTextField
 from wagtail.models import Orderable, Page
 
 
 class DogImage(Orderable):
     page = ParentalKey("Dog", on_delete=models.CASCADE, related_name="images")
     image = models.ForeignKey("wagtailimages.Image", on_delete=models.CASCADE, related_name="+")
-    # caption = models.CharField(blank=True, max_length=250)
 
     panels = [
         FieldPanel("image"),
-        # FieldPanel('caption'),
     ]
 
 
 class Dog(Page):
-    dob = models.DateField("Date of Birth")
-    # images = models.ManyToManyField("wagtailimages.Image", blank=True)
+    body = RichTextField()
 
     content_panels = Page.content_panels + [
-        FieldPanel("dob"),
+        FieldPanel("body"),
         InlinePanel("images", label="Images"),
-        # MultipleChooserPanel('images', chooser_field_name="image"),
     ]
