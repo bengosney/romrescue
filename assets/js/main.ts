5c040591ee06dfdf1e3f13e5c3d7a6f4de42d656
Ben Gosney <bengosney@googlemail.com>
2024-09-20 20:50:23 +0100

    feat(js): move the nav js into it's own file

--- assets/js/main.ts
+++ assets/js/main.ts
@@ -1,40 +1,5 @@
+import { initNav } from "./lib/nav";
+
 document.addEventListener('DOMContentLoaded', () => {
-    const anchorLinks = document.querySelectorAll('a[href^="#"]');
-    const firstId = anchorLinks[0]?.getAttribute('href');
-    anchorLinks.forEach(anchor => {
-        anchor.addEventListener('click', (event) => {
-            event.preventDefault();
-            const targetId = anchor.getAttribute('href');
-            if (targetId) {
-                const element = document.querySelector(targetId);
-                if (element) {
-                    element.scrollIntoView({ behavior: 'smooth' });
-                    const uri = firstId == targetId ? '/' : `${targetId}`;
-                    history.replaceState(undefined, '', uri)
-                }
-            }
-        });
-    });
-
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach(entry => {
-            const id = `#${entry.target.getAttribute('id')}`;
-            if (entry.isIntersecting) {
-                anchorLinks.forEach(anchor => {
-                    if (anchor.getAttribute('href') === id) {
-                        anchor.classList.add('active');
-                    } else {
-                        anchor.classList.remove('active');
-                    }
-                });
-                const uri = firstId == id ? '/' : id;
-                history.replaceState(undefined, '', uri);
-            }
-        });
-    }, { threshold: 0.5 });
-
-    const sections = document.querySelectorAll('section[id]');
-    sections.forEach(section => {
-        observer.observe(section);
-    });
+    initNav();
 });
